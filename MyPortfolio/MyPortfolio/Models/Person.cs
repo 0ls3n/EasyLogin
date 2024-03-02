3b89d28d4fce5889c1c797a92a1c31dcaea46a3c
Rasmus Olsen <rasmus782@gmail.com>
2024-03-03 00:13:32 +0100

    Created the 3 domain classes in C#

---
+++ MyPortfolio/MyPortfolio/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models
+{
+    public class Person
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+
+        public Person(string username, string password, string email, string displayName)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Email = email;
+            this.DisplayName = displayName;
+        }   
+    }
+}
---
+++ MyPortfolio/MyPortfolio/Models/Portfolio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Models
+{
+    public class Portfolio
+    {
+        public string Title { get; set; }
+        public Person Person { get; set; }
+        public List<Subject> SubjectList { get; set; }
+        
+        public Portfolio(string title, Person person)
+        {
+            this.Title = title;
+            this.Person = person;
+            SubjectList = new List<Subject>();
+        }
+    }
+}
