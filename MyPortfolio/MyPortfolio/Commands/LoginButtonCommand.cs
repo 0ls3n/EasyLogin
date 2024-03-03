f5447854d9f606759e3089bd43891156d2c9d573
Rasmus Olsen <rasmus782@gmail.com>
2024-03-03 22:16:39 +0100

    Created the icon for the application, and created the command for login

---
+++ MyPortfolio/MyPortfolio/Commands/LoginButtonCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MyPortfolio.ViewModels;
+
+namespace MyPortfolio.Commands
+{
+    internal class LoginButtonCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is LoginViewModel lvm)
+            {
+                lvm.Login();
+            }
+        }
+    }
+}
--- MyPortfolio/MyPortfolio/ViewModels/LoginViewModel.cs
+++ MyPortfolio/MyPortfolio/ViewModels/LoginViewModel.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 using MyPortfolio.Models;
+using MyPortfolio.Commands;
 
 namespace MyPortfolio.ViewModels
 {
     internal class LoginViewModel
     {
+        public string UsernameText { get; set; }
+        public string PasswordText { get; set; }
 
         private PersonRepository personRepository;
 
         public LoginViewModel() 
         {
             personRepository = new PersonRepository();
+        }
+
+        public ICommand LoginCommand { get; set; } = new LoginButtonCommand();
+
+        public void Login()
+        {
+            MessageBox.Show("Loggin in...");
         }
     }
 }
