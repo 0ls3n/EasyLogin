a2c68762b52f9d1f91bcfd64bd1f0eed7ea5009c
Rasmus Olsen <rasmus782@gmail.com>
2024-03-03 12:53:56 +0100

    Added NuGet Packages to csproj and updated the .gitignore

---
+++ MyPortfolio/MyPortfolio/Models/PersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace MyPortfolio.Models
+{
+    internal class PersonRepository
+    {
+    }
+}
