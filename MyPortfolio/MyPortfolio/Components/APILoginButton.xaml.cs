d782b8eb21f7871935e01bfef3d4fd02aa4b900e
Rasmus Olsen <rasmus782@gmail.com>
2024-03-08 21:35:23 +0100

    Added some tweaks to the design on the Microsoft Login button

---
+++ MyPortfolio/MyPortfolio/Components/APILoginButton.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPortfolio.Components
+{
+    /// <summary>
+    /// Interaction logic for APILoginButton.xaml
+    /// </summary>
+    public partial class APILoginButton : UserControl
+    {
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PasswordProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(APILoginButton),
+                new PropertyMetadata(string.Empty));
+
+        public APILoginButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
